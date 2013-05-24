890fb8c429eb6f5e419063ab8399fa1be6637051
Andrew Chin <andrew.chin@squareweave.com.au>
2013-05-24 22:30:15 +1000

    new files

---
+++ header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Cake Expo</title>
+	<meta name="author" content="Draftable">
+	<meta name="viewport" content="width-device-width, initial-scale=1.0">
+	
+	
+	<!-- vendor files -->
+	<link rel="stylesheet" href="design/vendor/bootstrap/css/bootstrap.css" type="text/css" media="screen" charset="utf-8">
+	<link rel="stylesheet" href="design/vendor/ss-pika/ss-pika.css" type="text/css" media="screen" charset="utf-8">
+	
+	<!-- muay styles -->
+	<link rel="stylesheet" href="design/css/layout.css" type="text/css" media="screen" charset="utf-8">
+	<link rel="stylesheet" href="design/vendor/bootstrap/css/bootstrap-responsive.css" type="text/css" media="screen" />
+	
+	
+	<!-- the javascriptage -->
+	<script src="http://code.jquery.com/jquery-latest.js"></script>
+	<script src="design/vendor/bootstrap/js/bootstrap.js" type="text/javascript" charset="utf-8"></script>
+	<script src="design/vendor/ss-pika/ss-pika.js" type="text/javascript" charset="utf-8"></script>
+	<script src="design/vendor/ss-standard/ss-standard.js" type="text/javascript" charset="utf-8"></script>
+	<script src="design/vendor/scrollto.js" type="text/javascript" charset="utf-8"></script>
+	<script src="design/scripts/toys.js" type="text/javascript" charset="utf-8"></script>
+	
+	<!-- google fonts -->
+	<link href='http://fonts.googleapis.com/css?family=Noto+Serif:400,700,400italic,700italic|Roboto:400,300,500,100' rel='stylesheet' type='text/css'>
+
+	
+</head>
+
+<body>
---
+++ index.php
@@ -0,0 +1,52 @@
+<?php include 'header.php'; ?>
+
+<div class="hero">
+	<div class="container">
+		
+		<div class="navbar">
+		  <div class="navbar-inner">
+		    <div class="container">
+		 
+		      <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+		      <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+		        <i class="icon ss-rows"></i> 
+		      </a>
+		 
+		      <!-- Be sure to leave the brand out there if you want it shown -->
+		      <a class="brand" href="#"><img src="design/images/logo.png" alt="logo" /></a>
+		 
+		      <!-- Everything you want hidden at 940px or less, place within here -->
+		      <div class="nav-collapse collapse">
+						<ul class="nav">
+							<li><a href="#">About</a></li>
+							<li><a href="#">Details</a></li>
+							<li><a href="#">Interested?</a></li>
+						</ul>
+						<ul class="nav pull-right">
+							<li><a href="#"><i class="icon ss-social-circle facebook"></i></a></li>
+						</ul>
+		      </div>
+		 
+		    </div>
+		  </div>
+		</div>
+		
+		<div class="row">
+			<div class="span8 offset4">
+				<div class="intro">
+					<h1>Love <em>making cakes</em>, <em>eating cakes</em> or just want to <em>learn a thing</em> or two about <em>sugar art</em> from the professionals?</h1>
+					<p class="lead">We look to kick off a number of cake expos throughout Australia at the end of 2013 with participation from leading cake and cupcake stores, cake decorating equipment stores, cake decorating schools and more!</p>
+					<p>Come along to learn how the professionals create their edible masterpieces whilst having a slice (or two) while you’re there.
+					</p>
+					<p>Like us on Facebook if you would like to be kept up to date with our latest moves and where you will be able to purchase tickets for the events or register your interest to participate as a partner or vendor below.
+					</p>
+					<p>Excited yet?  We are  :)</p>
+				</div>
+			</div>
+		</div>
+
+		
+		
+</div> <!-- hero -->
+
+<?php include 'footer.php'; ?>
